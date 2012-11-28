c862c0149fa4cb35666168e67b63c932945740be
wiza <wizawu@gmail.com>
2012-11-28 19:41:26 +0800

    douban_api api added

--- src/douban_api.erl
+++ src/douban_api.erl
@@ -24,26 +24,80 @@
 
 -behaviour(gen_server).
 
--export([start_link/0]).
+-export([start_link/3,
+         access_token/1, refresh_token/1,
+         home_timeline/2, user_timeline_2,
+         update/2, reshare/2, delete/2,
+         comment/2, comment_list/1, 
+       ]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--record(state, {appkey, secret, uri, 
-                atoken, rtoken, userid, expire}).
+-record(state, {appkey, secret, uri, table}). 
 
 %%-----------------------------------------------------------------------------
 %%  API
 %%-----------------------------------------------------------------------------
-start_link() ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+start_link(AppKey, Secret, URI) ->
+    gen_server:start_link(?MODULE, [AppKey, Secret, URI], []).
 
+access_token(Code) -> 
+    gen_server:call(?MODULE, {access_token, Code}).
+
+refresh_token(Code) ->
+    gen_server:call(?MODULE, {refresh_token, Code}).
+
+% home_timeline(Token, [1, 50]) will return 50 newest statuses
+home_timeline(Token, [A, B]) ->
+    gen_server:call(?MODULE, {home_timeline, Token, [A, B]}).
+
+user_timeline(UserId, [A, B]) ->
+    gen_server:call(?MODULE, {user_timeline, UserId, [A, B]}).
+
+% Content -> [content()]
+% content() -> {text, Text}   | {image, Bytes} | 
+%              {title, Title} | {url, Url}     | {desc, Desc}
+update(Code, Content) when is_list(Content) ->
+    gen_server:call(?MODULE, {update, Code, Content}).
+    
+reshare(Code, StatId) ->
+    gen_server:call(?MODULE, {reshare, Code, StatId}).
+    
+delete(Code, StatId) ->
+    gen_server:call(?MODULE, {delete, StatId}).
+
+comment(Code, Text) ->
+    gen_server:call(?MODULE, {comment, Code, Text}).
+
+commmet_list(StatId) ->
+    gen_server:call(?MODULE, {comment_list, StatId}).
+
+
+
+delete_comment(Code, CommId) ->
+    gen_server:call(?MODULE, {delete_comment, Code, CommId}).
+
+follow(Code, UserId) ->
+    gen_server:call(?MODULE, {follow, Code, UserId}).
+
+unfollow(Code, UserId) ->
+    gen_server:call(?MODULE, {unfollow, Code, UserId}).
+
+block(Code, UserId) ->
+    gen_server:call(?MODULE, {block, Code, UserId}).
+
+search_user(Text) ->
+    gen_server:call(?MODULE, {search_user, Text}).
 
 %%-----------------------------------------------------------------------------
 %%  Callback
 %%-----------------------------------------------------------------------------
-init([]) ->
-    {ok, #state{}}.
+init([AppKey, Secret, URI]) ->
+    ok = inets:start(),
+    ssl:start(),
+    T = ets:new(anonyms, [set, private]),
+    {ok, #state{appkey=AppKey, secret=Secret, uri=URI, table=T}}.
 
 handle_call(_Request, _From, State) ->
     Reply = ok,
