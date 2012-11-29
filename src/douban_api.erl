49cc46da7f401ce4742590323d083075984a2b53
wiza <wizawu@gmail.com>
2012-11-29 20:09:12 +0800

    home_timeline ok

--- src/douban_api.erl
+++ src/douban_api.erl
@@ -29,20 +29,20 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--record(state, {appkey, secret, uri}). 
+-record(state, {apikey, secret, uri}). 
 
--define(HttpAPI, "http://api.douban.com/v2").
--define(HttpsAPI, "https://api.douban.com/v2").
+-define(HttpApi, "http://api.douban.com/").
+-define(HttpsApi, "https://api.douban.com/").
 -define(TokenTbl, douban_token_table).
 
 %%-----------------------------------------------------------------------------
 %%  API
 %%-----------------------------------------------------------------------------
-start_link({AppKey, Secret, Uri}) ->
-    start_link(?MODULE, {AppKey, Secret, Uri}).
+start_link({Apikey, Secret, Uri}) ->
+    start_link(?MODULE, {Apikey, Secret, Uri}).
 
-start_link(Server, {AppKey, Secret, Uri}) when is_atom(Server) ->
-    gen_server:start_link({local,Server}, ?MODULE, [AppKey,Secret,Uri], []).
+start_link(Server, {Apikey, Secret, Uri}) when is_atom(Server) ->
+    gen_server:start_link({local,Server}, ?MODULE, [Apikey,Secret,Uri], []).
 
 call(Request) when is_tuple(Request) ->
     gen_server:call(?MODULE, Request).
@@ -53,7 +53,7 @@ call(Server, Request) when is_tuple(Request) ->
 %%-----------------------------------------------------------------------------
 %%  Callback
 %%-----------------------------------------------------------------------------
-init([AppKey, Secret, Uri]) ->
+init([Apikey, Secret, Uri]) ->
     inets:start(),
     ssl:start(),
     case lists:member(?TokenTbl, ets:all()) of
@@ -62,12 +62,13 @@ init([AppKey, Secret, Uri]) ->
         false -> ets:new(?TokenTbl, [named_table, set, public]);
         true -> pass
     end,
-    {ok, #state{appkey=AppKey, secret=Secret, uri=Uri}}.
+    {ok, #state{apikey=Apikey, secret=Secret, uri=Uri}}.
 
+%%-----------------------------------------------------------------------------
 handle_call({access_token, Code}, _From, State) ->
-    #state{appkey=AppKey, secret=Secret, uri=Uri} = State,
+    #state{apikey=Apikey, secret=Secret, uri=Uri} = State,
     Url = "https://www.douban.com/service/auth2/token",
-    Body = "client_id=" ++ AppKey ++ "&client_secret=" ++ Secret ++ 
+    Body = "client_id=" ++ Apikey ++ "&client_secret=" ++ Secret ++ 
            "&redirect_uri=" ++ Uri ++ "&grant_type=authorization_code&code=" 
            ++ Code,
     Result = request(post, Url, Body), 
@@ -78,12 +79,12 @@ handle_call({access_token, Code}, _From, State) ->
     {reply, Reply, State};
 
 handle_call({refresh_token, Code}, _From, State) ->
-    #state{appkey=AppKey, secret=Secret, uri=Uri} = State,
+    #state{apikey=Apikey, secret=Secret, uri=Uri} = State,
     case ets:lookup(?TokenTbl, Code) of
         [] -> handle_call({access_token, Code}, self(), State);
         [{Code, {_, _, Rtk}}] -> 
             Url = "https://www.douban.com/service/auth2/token",
-            Body = "client_id=" ++ AppKey ++ "&client_secret=" ++ 
+            Body = "client_id=" ++ Apikey ++ "&client_secret=" ++ 
                    Secret ++ "&redirect_uri=" ++ Uri ++ 
                    "&grant_type=refresh_token&refresh_token=" ++ Rtk,
             Result = request(post, Url, Body),
@@ -92,8 +93,28 @@ handle_call({refresh_token, Code}, _From, State) ->
                 Json -> save_token(Code, Json)  % {json, Json}
             end,
             {reply, Reply, State}
-    end.
+    end;
 
+handle_call({home_timeline, Code, N}, _From, State) ->
+    Reply = case ets:lookup(?TokenTbl, Code) of
+        [] -> no_access_token;
+        [{Code, {_, Atk, _}}] ->
+            request(Atk, ?HttpsApi ++ "shuo/v2/statuses/home_timeline"
+                    ++ "?count=" ++ integer_to_list(N))
+    end,
+    {reply, Reply, State};
+
+handle_call({home_timeline, Code, MaxId, N}, _From, State) ->
+    Reply = case ets:lookup(?TokenTbl, Code) of
+        [] -> no_access_token;
+        [{Code, {_, Atk, _}}] ->
+            request(Atk, ?HttpsApi ++ "shuo/v2/statuses/home_timeline"
+                    ++ "?count=" ++ integer_to_list(N)
+                    ++ "&until_id=" ++ integer_to_list(MaxId))
+    end,
+    {reply, Reply, State}.
+
+%%-----------------------------------------------------------------------------
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
@@ -109,15 +130,22 @@ code_change(_OldVsn, State, _Extra) ->
 %%-----------------------------------------------------------------------------
 %%  Internal
 %%-----------------------------------------------------------------------------
-request(get, Url) ->
-    httpc:request(get, {Url,[]}, [], []).
-
 request(post, Url, Body) ->
     Result = httpc:request(post, 
              {Url,[],"application/x-www-form-urlencoded",Body},
              [], [{full_result,false}]),
     case Result of
-        {ok,{200, Json}} -> Json;
+        {ok, {200, Json}} -> Json;
+        {ok, X} -> {error, X};
+        X -> {error, X}
+    end.
+
+request(Token, Url) ->
+    Header = [{"Authorization", "Bearer "++Token}],
+    Result = httpc:request(get, {Url,Header}, [], [{full_result,false}]),
+    case Result of
+        {ok, {200, Json}} -> {json, Json};
+        {ok, X} -> {error, X};
         X -> {error, X}
     end.
 
